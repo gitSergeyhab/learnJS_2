447fc4462b0fc0ac04de8e73ebb251c01e06de6c
Sergey <redmoloch@yanadex.ru>
2020-12-24 16:33:05 +0300

    localStorage and makeRedNoDigit

--- js/scriptTabs.js
+++ js/scriptTabs.js
@@ -364,8 +364,9 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 
     // calculator
-    let sex = -161;
-    let act = 1.375;
+
+
+    
     const genderId = document.querySelector('#gender');
     const genders = genderId.querySelectorAll('.calculating__choose-item');
     const activityBig = document.querySelector('.calculating__choose_big');
@@ -374,21 +375,55 @@ window.addEventListener('DOMContentLoaded', () => {
     const heightFild = document.querySelector('#height');
     const ageFild = document.querySelector('#age');
     const resultCalc = document.querySelector('.calculating__result span');
+    let sex, act;
+
+    if (localStorage.getItem('sex')) {
+        sex = +localStorage.getItem('sex');
+        genderRemove(genders);
+        genders.forEach(gender => {
+            if (gender.getAttribute('data-sex') == localStorage.getItem('sexAtr')) {
+                gender.classList.add('calculating__choose-item_active')
+            }
+        })
+    } else {
+      sex = -161;  
+    }
+    
+    if (localStorage.getItem('act')) {
+        act = +localStorage.getItem('act');
+        genderRemove(activities);
+        activities.forEach(activity => {
+            if (+activity.getAttribute('data-act') == localStorage.getItem('act')) {
+                activity.classList.add('calculating__choose-item_active')
+            }
+        })
+    } else {
+       act = 1.375; 
+    }
 
     weightVal = 0; heightVal = 0; ageVal = 0;
     
+    function noDigitMakeRed(field) {
+        if (field.value.match(/\D/g)) {
+            field.style.border = '1px solid red';
+        }
+    }
+
     weightFild.addEventListener('input', () => {
-        weightVal = +weightFild.value    
+        noDigitMakeRed(weightFild);
+        weightVal = +weightFild.value;    
         showCallories();
     })
 
     heightFild.addEventListener('input', () => {
-        heightVal = +heightFild.value    
+        noDigitMakeRed(heightFild);
+        heightVal = +heightFild.value;    
         showCallories();
     })
 
     ageFild.addEventListener('input', () => {
-        ageVal = +ageFild.value    
+        noDigitMakeRed(ageFild);
+        ageVal = +ageFild.value;    
         showCallories();
     })
    
@@ -403,8 +438,12 @@ window.addEventListener('DOMContentLoaded', () => {
             genderRemove(genders);
             gender.classList.add('calculating__choose-item_active');
             if (gender.getAttribute('data-sex') == 'f') {
+                localStorage.setItem('sex', -161);
+                localStorage.setItem('sexAtr', 'f');
                 sex = -161;
             } else {
+                localStorage.setItem('sex', 5);
+                localStorage.setItem('sexAtr', 'm');
                 sex = 5;
             }
             showCallories();
@@ -416,6 +455,7 @@ window.addEventListener('DOMContentLoaded', () => {
             genderRemove(activities);
             activity.classList.add('calculating__choose-item_active')
             act = +activity.getAttribute('data-act');
+            localStorage.setItem('act', act)
             showCallories();
         })
     })
